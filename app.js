ce513add83a878a1db93492e2269c3704b8b2529
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2025-02-23 07:46:55 +0200

    repositioned well the errorMiddleware below the routes for its function to take the effect.

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionsRouter from "./routes/subscriptions.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+// Middleware Section
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionsRouter)
+
+app.use(errorMiddleware)
 
 app.listen(PORT, async () =>{
     console.log(`Subscriptions Tracker Api running on ${PORT}`);
